39adfd504cbd6f0563ba5b2d6f361a44156c2662
L4FeeR <muhamedlafeer837@gmail.com>
2024-07-22 23:12:10 +0530

    synced

--- install.sh
+++ install.sh
@@ -9,15 +9,43 @@ bpkg=("htop" "sdfsfs" "neofetch" "gh" "git" "wget" "curl" "openssh" "tmate" "tmu
 #kernelpkg
 kpkg=("bc" "flex" "make" "clang" "build-essential")
 
+#colors
+red="\033[1;31m"
+blue="\033[1;34m"
+green="\033[1;32m"
+grey="\033[1;90m"
+yellow="\033[1;93m"
+purple="\033[1;35m"
+brown="\033[1;33m"
+cyan="\033[1;36m"
+reset="\033[1;0m"
+blink="\033[1;5m"
+us="\033[1;4m"
+bold="\033[1;1m"
 
+
+#echo -e "$red red $blue blue $green green $yellow yellow $reset reset $blink blink $reset $us us $bold bold"
+
+function pkgm () {
+        if [ $(command -v apt) ];then
+                apt install $1 -y
+        elif [ $(command -v pacman) ];then
+                pacman -Sy $1
+        elif [ $(command -v dnf) ];then
+                dnf -s $1
+        else
+                echo -e "$blue[$red-$blue]$yellow Cannot Find Package Manager!"
+        fi
+        }
 function install_pkg () {
 for pkg in ${cpkg[@]}; do
   f=`command -v $pkg`
-  echo -en "$pkg\t\t---->\t\t"
+  echo -en "$bold $pkg\t\t $purple ---->\t\t"
   if [ $f ];then
-	  echo "Found!"
+	  echo "$green Found!"
   else
-	  echo "Not Found!"
+	  echo -e "$red Not Found!"
+	  pkgm $pkg
   fi
 	done
 }
