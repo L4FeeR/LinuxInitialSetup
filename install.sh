ae76acb9dabe7809aa79273bcac8fec1ff667d18
L4FeeR <muhamedlafeer837@gmail.com>
2024-07-22 21:53:11 +0530

    added sample code template.

--- install.sh
+++ install.sh
@@ -1,3 +1,74 @@
 #!/usr/bin/bash
 
-#Automatic linux setup tool
+#Automatic Linux setup scripelif [ $choice -eq 2 ];then
+
+
+#basic pkg
+bpkg=("htop" "sdfsfs" "neofetch" "gh" "git" "wget" "curl" "openssh" "tmate" "tmux" "python3")
+
+#kernelpkg
+kpkg=("bc" "flex" "make" "clang" "build-essential")
+
+
+function install_pkg () {
+for pkg in ${cpkg[@]}; do
+  f=`command -v $pkg`
+  echo -en "$pkg\t\t---->\t\t"
+  if [ $f ];then
+	  echo "Found!"
+  else
+	  echo "Not Found!"
+  fi
+	done
+}
+
+install_pkg
+
+function main() {
+echo "		Installer Menu"
+echo ""
+echo "		1. Base Packages"
+echo "		2. Kernel Build Packages"
+echo "		3. Ollama -The Offline AI"
+echo "		4. Add shortcuts"
+echo "		5. Install All Shells"
+echo "		o  Exit"
+echo ""
+read -p "[+]Enter your choice : " choice
+
+
+if [ $choice -eq 1 ];then
+	cpkg=${bpkg[@]}
+	install_pkg
+	main
+elif [ $choice -eq 2 ];then
+	cpkg=${kpkg[@]}
+	install_pkg
+	main
+elif [ $choice -eq 3 ];then
+	cd $HOME && ls
+	main
+else
+	echo "[-]Exiting from the script!"
+	return 0
+fi
+
+
+
+
+
+function install_pkg () {
+for pkg in ${cpkg[@]}; do
+  f=`command -v $pkg`
+  echo -en "$pkg\t\t---->\t\t"
+  if [ $f ];then
+	  echo "Found!"
+  else
+	  echo "Not Found!"
+  fi
+  sleep 1
+	done
+}
+
+}
+main
